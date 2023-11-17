2af1cb6252232d276e67803a0b12ad90005897f9
Rafael Botia Ordaz <rafabotiaordaz@gmail.com>
2023-11-17 17:39:05 +0100

    3. Rutas con Laravel

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,25 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Home';
+});
+
+Route::get('/usuarios', function () {
+    return 'Ususarios';
+});
+
+Route::get('/usuarios/nuevo', function () {
+    return "Crear nuevo usuario";
+});
+
+Route::get('/usuarios/{id}', function ($id) {
+    return "Mostrando detalle del usuario: {$id}";
+})->where('id', '[0-9]+');
+
+Route::get('/saludo/{name}/{nickname?}', function ($name, $nickname = null) {
+    if ($nickname) {
+        return "Bienvenido {$name}, tu apodo es {$nickname}";
+    } else {
+        return "Bienvenido {$name}";
+    }
 });
