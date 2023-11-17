3ddb6d108d5b48fa6082ed74a5bf7f8a7156f6d7
Rafael Botia Ordaz <rafabotiaordaz@gmail.com>
2023-11-17 19:59:02 +0100

    6. Vistas

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,7 +8,18 @@ class UserController extends Controller
 {
     public function index()
     {
-        return 'Usuarios';
+        $users = [
+            'Joel',
+            'Ellie',
+            'Tess',
+            'Tommy',
+            'Bill',
+            '<script>Hola</script>'
+        ];
+
+        $title = 'Listado de usuarios';
+
+        return view('users',compact('users', 'title'));
     }
 
     public function show($id)
---
+++ resources/views/users.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listado de usuarios - Styde.net</title>
+</head>
+<body>
+    <h1><?= e($title); ?></h1>
+
+    <ul>
+        <?php foreach($users as $user): ?>
+            <li><?= e($user); ?></li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+</html>
